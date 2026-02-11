d708aaa74b36767dc6e4edb2227719ed28507298
David Camargo <davidcp90@gmail.com>
2026-02-11 01:31:31 -0500

    Test linting hook

---
+++ backend/test-lint.ts
@@ -0,0 +1,8 @@
+// Test file with intentional linting issues
+const unusedVariable = 'test'
+const messyCode   =   {  foo:   'bar'   }
+
+function badFormatting( )
+{
+console.log( "bad formatting" )
+}
